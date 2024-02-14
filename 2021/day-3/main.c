1b4c978227d54952f93dc23616676d2fa22a83ec
Camila Alvarez <cam.alvarez.i@gmail.com>
2024-02-14 11:19:08 -0300

    Day 3, part2. The code could be refactored

--- 2021/day-3/main.c
+++ 2021/day-3/main.c
@@ -21,42 +21,112 @@
 #include <stdbool.h>
 #include <math.h>
 
+#define MAX_LENGTH_BITS 256
+#define MAX_ENTRIES 1000
+
+int to_decimal(char *binary, int binary_len) {
+    int number = 0;
+    for (int i = 0; i < binary_len; i++){
+        char digit = binary[i];
+        if (digit == '1'){
+            number += (int)pow(2, binary_len-i-1);
+        }
+    }
+    return number;
+}
+
 int main(int argc, char const *argv[]){
-    int number_columns = 0;
-    int* number_zeros, *number_ones;
     size_t read = 0, n;
     char *line = NULL;
     bool first_row = true;
+    char **lines = malloc(sizeof(char[MAX_LENGTH_BITS])*MAX_ENTRIES);
+    for(int i = 0; i < MAX_ENTRIES; i++) {
+        lines[i] = malloc(sizeof(char)*MAX_LENGTH_BITS);
+    }
+    char **lines2 = malloc(sizeof(char[MAX_LENGTH_BITS])*MAX_ENTRIES);
+    for(int i = 0; i < MAX_ENTRIES; i++) {
+        lines2[i] = malloc(sizeof(char)*MAX_LENGTH_BITS);
+    }
+    char **zeroes = malloc(sizeof(char[MAX_LENGTH_BITS])*MAX_ENTRIES);
+    for(int i = 0; i < MAX_ENTRIES; i++) {
+        zeroes[i] = malloc(sizeof(char)*MAX_LENGTH_BITS);
+    }
+    char **ones = malloc(sizeof(char[MAX_LENGTH_BITS])*MAX_ENTRIES);
+    for(int i = 0; i < MAX_ENTRIES; i++) {
+        ones[i] = malloc(sizeof(char)*MAX_LENGTH_BITS);
+    }
     FILE *file = fopen(argv[1], "r");
-    size_t row_len;
+    size_t row_len, number_lines = 0;
     while((read = getline(&line, &n, file)) != -1){
         line[strcspn(line, "\n")] = 0;
         if (first_row){
             row_len = strlen(line);
-            number_zeros = calloc(row_len, sizeof(int));
-            number_ones = calloc(row_len, sizeof(int));
             first_row = false;
         }
-        for(int i = 0; i < row_len; i++){
-            if (line[i] == '0') {
-                number_zeros[i]++;
-            } else if (line[i] == '1') {
-                number_ones[i]++;
-            }
-        }
-    }
-    int gamma = 0, epsilon = 0;
-    // we're assuming there won't be an equal number of ones and zeros
-    for (int i = 0; i < row_len; i++){
-        if (number_zeros[i] > number_ones[i]){
-            epsilon += (int)pow(2, row_len - 1 - i);
-        } else {
-            gamma += (int)pow(2, row_len - 1 - i);
-        }
+        strcpy(lines[number_lines], line);
+        strcpy(lines2[number_lines], line);
+        number_lines++;
     }
     free(line);
-    printf("Gamma: %d\n", gamma);
-    printf("Epsilon: %d\n", epsilon);
-    printf("Result: %d\n", gamma * epsilon);
+    int bit = 0, number_results = number_lines;
+    while(bit < row_len && number_results > 1){
+        int nz = 0, no = 0;
+        for (int i = 0; i < number_results; i++) {
+            if (lines[i][bit] == '0') {
+                strcpy(zeroes[nz], lines[i]);
+                nz++;
+            } else if (lines[i][bit] == '1'){
+                strcpy(ones[no], lines[i]);
+                no++;
+            }
+        }
+        char **aux;
+        if(no >= nz) {
+            aux = lines;
+            lines = ones;
+            ones = aux;
+            number_results = no;
+        } else if (no < nz) { 
+            aux = lines;
+            lines = zeroes;
+            zeroes = aux;
+            number_results = nz;
+        }
+        bit++;
+    }
+    bit = 0, number_results = number_lines;
+    while(bit < row_len && number_results > 1){
+        int nz = 0, no = 0;
+        for (int i = 0; i < number_results; i++) {
+            if (lines2[i][bit] == '0') {
+                strcpy(zeroes[nz], lines2[i]);
+                nz++;
+            } else if (lines2[i][bit] == '1'){
+                strcpy(ones[no], lines2[i]);
+                no++;
+            }
+        }
+        char **aux;
+        if(no < nz) {
+            aux = lines2;
+            lines2 = ones;
+            ones = aux;
+            number_results = no;
+        } else if (no >= nz) { 
+            aux = lines2;
+            lines2 = zeroes;
+            zeroes = aux;
+            number_results = nz;
+        }
+        bit++;
+    }
+    printf("Oxygen rating: %s\n", lines[0]);
+    printf("CO2 rating: %s\n", lines2[0]);
+    int oxygen = to_decimal(lines[0], row_len), co2 = to_decimal(lines2[0], row_len);
+    printf("Oxygen (%d) x CO2 (%d) = %d\n", oxygen, co2, oxygen*co2);
+    free(lines);
+    free(lines2);
+    free(zeroes);
+    free(ones);
     return 0;
 }
